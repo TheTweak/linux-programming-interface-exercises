c2e32b282f00ca13e9942d3e3eeda28db8090e99
TheTweak <tweakon@gmail.com>
2022-01-11 18:09:49 +0300

    6-processes: exercise 3 finished

--- 6-processes/6-3-setenv.c
+++ 6-processes/6-3-setenv.c
@@ -14,6 +14,35 @@ int setenv(const char *name, const char *value, int overwrite) {
 		strcat(nameValue, value);
 		putenv(nameValue);
 	}
+	return 0;
+}
+
+int unsetenv(const char *name) {
+	char **p = environ;
+	bool deleted = 0; 
+	for (; *p != NULL; p++) {
+		char *equalSignPtr = strchr(*p, '=');
+		size_t equalSignIndex = equalSignPtr - *p;
+		char *pName = strndup(*p, equalSignIndex);
+		if (strcmp(pName, name) == 0) {
+			printf("pName is %s (%s), removing %s\n", pName, *p, name);
+			deleted = 1;
+			break;
+		}
+	}
+	if (deleted) {
+		for (; *p != NULL; p++) {
+			*p = *(p + 1);
+		}
+	}
+	return 0;
+}
+
+void printEnv() {
+	printf("\n");
+	for (char **p = environ; *p != NULL; p++) {
+		printf("%s\n", *p);
+	}	
 }
 
 int main(int argc, char *argv[]) {
@@ -28,5 +57,29 @@ int main(int argc, char *argv[]) {
 	setenv("qwer", "ghjk", 1);
 	if (strcmp(getenv("qwer"), "ghjk") != 0) {
 		errExit("setenv with overwrite and new value");
+	}
+	
+	setenv("qwerty", "value", 0);
+	printEnv();
+	unsetenv("qwer");
+	printf("\n*********After unset(qwer)\n");
+	printEnv();
+	if (getenv("qwer") != NULL) {
+		errExit("Unset env single value");
+	}
+	setenv("asd", "qwe", 0);
+	setenv("asd", "hjk", 0);
+	printf("\n*********Before unset(asd)\n");
+	printEnv();
+	unsetenv("asd");
+	printf("\n*********After unset(asd)\n");
+	printEnv();
+	if (getenv("asd") != NULL) {
+		errExit("Unset env multiple values");
+	}
+	char *qwerty = getenv("qwerty");
+	printf("qwerty value is %s\n", qwerty);
+	if (qwerty == NULL ||  strcmp(getenv("qwerty"), "value") != 0) {
+		errExit("Unset removed more than needed");
 	}
 }
