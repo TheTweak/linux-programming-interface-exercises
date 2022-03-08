0e494eb8ba3758101da7b9e8d5ee0e8a0549196b
TheTweak <tweakon@gmail.com>
2022-03-08 23:47:02 +0300

    12 system and process information: exercise 12-1 ps - refactor into separate functions

--- 12-system-and-process-information/12-1-ps.c
+++ 12-system-and-process-information/12-1-ps.c
@@ -7,68 +7,85 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-	if (argc != 2)
-	{
+ssize_t readFile(int fd, char **dest, int destSize) {
+	ssize_t bufferSize = 128;
+	char buffer[bufferSize];  
+	char *ptr = *dest;
+	ssize_t size = destSize;
+	ssize_t readBytes = 0;
+	ssize_t totalRead = 0;
+	while ((readBytes = read(fd, buffer, bufferSize)) != 0) {
+		totalRead += readBytes;
+		if (totalRead > size) {
+			char *newLineBytes = malloc(size*2);
+			memcpy(newLineBytes, *dest, size);
+			dest = &newLineBytes;
+			ptr = *dest + size;
+			size = size*2;
+		}
+		memcpy(ptr, &buffer, readBytes);
+		ptr += readBytes;
+	}
+	return totalRead;
+}
+
+char* constructStatusDirPath(struct dirent *dirEnt) {
+	const char *statusDir = "/proc/status";
+	const char* dirName = dirEnt->d_name;
+	int arraySize = strlen(statusDir) + strlen(dirName) + 2;
+	char *statusDirName = malloc(arraySize);
+	for (int i = 0; i < arraySize; i++) {
+		statusDirName[i] = '\0';
+	}
+	strcat(statusDirName, "/proc/");
+	strcat(statusDirName, dirName);
+	strcat(statusDirName, "/status");
+	return statusDirName;
+}
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
 		printf("Usage: ps <user-name>\n");
 		return 0;
 	}
 
 	const char *userName = argv[1];
 	struct passwd *pwd = getpwnam(userName);
-	if (pwd == NULL)
-	{
+	if (pwd == NULL) {
 		printf("Failed to get password record for user %s\n", userName);
 		return 1;
 	}
 	printf("UID: %d\n", pwd->pw_uid);
 
 	DIR *procDir = opendir("/proc");
-	if (procDir == NULL)
-	{
+	if (procDir == NULL) {
 		printf("Failed to opendir\n");
 		return 1;	
 	}
 	
 	struct dirent *dirEnt;
-	const char *statusDir = "/proc/status";
-	while ((dirEnt = readdir(procDir)) != NULL)
-	{
-		const char* dirName = dirEnt->d_name;
-		int arraySize = strlen(statusDir) + strlen(dirName) + 2;
-		char statusDirName[arraySize];
-		for (int i = 0; i < arraySize; i++) {
-			statusDirName[i] = '\0';
-		}
-		strcat(statusDirName, "/proc/");
-		strcat(statusDirName, dirName);
-		strcat(statusDirName, "/status");
+	while ((dirEnt = readdir(procDir)) != NULL) {
+		char *statusDirName = constructStatusDirPath(dirEnt);
 		int fd;
-		ssize_t initialSize = 512;
-		ssize_t bufferSize = 128;
-		char buffer[bufferSize];  
 		if ((fd = open(statusDirName, O_RDONLY)) != -1) {
 			printf("\n\nstatus dir: %s \n", statusDirName);
-			char *lineBytes = malloc(initialSize);
-			char *ptr = lineBytes;
-			ssize_t size = initialSize;
-			ssize_t readBytes = 0;
-			ssize_t totalRead = 0;
-			while ((readBytes = read(fd, buffer, bufferSize)) != 0) {
-				totalRead += readBytes;
-				if (totalRead > size) {
-					char *newLineBytes = malloc(size*2);
-					memcpy(newLineBytes, lineBytes, size);
-					free(lineBytes);
-					lineBytes = newLineBytes;
-					ptr = lineBytes + size;
-					size = size*2;
+			int initialSize = 512;
+			char *content = malloc(initialSize);
+			ssize_t totalRead = readFile(fd, &content, initialSize);
+			//write(1, content, totalRead);
+			close(fd);
+			write(1, content+0, 20);
+			/*
+			ssize_t l = 0;
+			ssize_t r = 0;
+			while (r < totalRead) {
+				while (r < totalRead && *(content+r) != '\n') {
+					r++;
 				}
-				memcpy(ptr, &buffer, readBytes);
-				ptr += readBytes;
-			}
-			write(1, lineBytes, totalRead);
+				write(1, content + l, r);
+				l = r;
+			}*/
+			break;
 		}
 	}
 }
