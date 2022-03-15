0c5df9c6009bbeddc882168a0c14fb1d4451250f
TheTweak <tweakon@gmail.com>
2022-03-15 23:05:35 +0300

    12 system and process information: exercise 12-2 - ps tree

--- 12-system-and-process-information/12-2-pstree.c
+++ 12-system-and-process-information/12-2-pstree.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
 
 typedef struct {
 	int id;
@@ -13,6 +18,10 @@ void printIdent(int level) {
 	}	
 }
 
+static int cmpProcess(const void *p1, const void *p2) {
+	return ((const Process *)p1)->id - ((const Process *)p2)->id;
+}
+
 void printChildren(int level, Process *proc, Process *procs, size_t procCount) {
 	printIdent(level);
 	printf("[%d] %s\n", proc->id, proc->name);
@@ -23,28 +32,103 @@ void printChildren(int level, Process *proc, Process *procs, size_t procCount) {
 		}
 		p++;
 	}
- }
+}
 
-int main(int argc, char *argv[]) {
-	Process procsArr[] = {
-		{ .id = 0, .parentId = -1, .name = "init" },
-		{ .id = 1, .parentId = 0, .name = "ls" },
-		{ .id = 2, .parentId = 0, .name = "java" },
-		{ .id = 3, .parentId = 2, .name = "thread0" },
-		{ .id = 4, .parentId = 2, .name = "thread1" },
-	};
+char* constructStatusDirPath(struct dirent *dirEnt) {
+	const char *statusDir = "/proc/status";
+	const char* dirName = dirEnt->d_name;
+	int arraySize = strlen(statusDir) + strlen(dirName) + 2;
+	char *statusDirName = malloc(arraySize);
+	for (int i = 0; i < arraySize; i++) {
+		statusDirName[i] = '\0';
+	}
+	strcat(statusDirName, "/proc/");
+	strcat(statusDirName, dirName);
+	strcat(statusDirName, "/status");
+	return statusDirName;
+}
 
-	size_t count = sizeof(procsArr)/sizeof(Process);
-	Process *procs = malloc(sizeof(Process)*count);
-	Process *p = procs;
-	for (size_t i = 0; i < count; i++) {
-		p->id = procsArr[i].id;
-		p->parentId = procsArr[i].parentId;
-		p->name = procsArr[i].name;
-		p++;
+ssize_t readFile(int fd, char **dest, int destSize) {
+	ssize_t bufferSize = 128;
+	char buffer[bufferSize];  
+	char *ptr = *dest;
+	ssize_t size = destSize;
+	ssize_t readBytes = 0;
+	ssize_t totalRead = 0;
+	while ((readBytes = read(fd, buffer, bufferSize)) != 0) {
+		totalRead += readBytes;
+		if (totalRead > size) {
+			char *newLineBytes = malloc(size*2);
+			memcpy(newLineBytes, *dest, size);
+			dest = &newLineBytes;
+			ptr = *dest + size;
+			size = size*2;
+		}
+		memcpy(ptr, &buffer, readBytes);
+		ptr += readBytes;
+	}
+	return totalRead;
+}
+
+char *readValue(char *key, char* content) {
+	char *keyColon = malloc(strlen(key) + 2);
+	strcpy(keyColon, key);
+	strcat(keyColon, ":");
+	char *keyStart;
+	if ((keyStart = strstr(content, keyColon)) != NULL) {
+		char *uid = strtok(keyStart, "\t");
+		uid = strtok(NULL, "\n");
+		return uid;
+	}
+}
+
+Process *getProcesses(size_t *countResult) {
+	DIR *procDir = opendir("/proc");
+	if (procDir == NULL) {
+		printf("Failed to opendir\n");
+		return NULL;	
 	}
 
-	printChildren(0, procs, procs, count);
+	size_t procCount = 5;
+	Process *result = malloc(sizeof(Process)*procCount);
+	Process *ptr = result;
+	struct dirent *dirEnt;
+	size_t i = 0;
+	while ((dirEnt = readdir(procDir)) != NULL) {
+		char *statusDirName = constructStatusDirPath(dirEnt);
+		int fd;
+		if ((fd = open(statusDirName, O_RDONLY)) != -1) {
+			int initialSize = 512;
+			char *content = malloc(initialSize);
+			readFile(fd, &content, initialSize);
+			close(fd);
 
-	free(procs);
+			if (i >= procCount) {
+				Process *newProc = malloc(sizeof(Process)*procCount*2);
+				memcpy(newProc, result, sizeof(Process)*procCount);
+				result = newProc;
+				procCount *= 2;
+				ptr = result + i;
+			}
+			char *pprocId = readValue("PPid", content);
+			char *procId = readValue("Pid", content);
+			char *procName = readValue("Name", content);
+			Process proc = {.id=atoi(procId), .parentId=atoi(pprocId), .name=procName};
+			*ptr = proc;
+			i++;
+			ptr++;
+		}
+	}
+	*countResult = i;
+	return result;
+}
+
+int main(int argc, char *argv[]) {
+	size_t count;
+	Process *procs = getProcesses(&count);
+	if (procs == NULL) {
+		return 1;
+	}
+	qsort(procs, count, sizeof(Process), cmpProcess);
+	printChildren(0, procs, procs, count);
 }
